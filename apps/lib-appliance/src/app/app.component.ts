22f844c17b86b54bdfccd8ba968d65b0945c59a6
CORP\jhernan6 <jose.hernandez-rizo-external@capgemini.com>
2021-05-20 11:53:17 +0200

    hand service separed from video

--- apps/lib-appliance/src/app/app.component.ts
+++ apps/lib-appliance/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AfterViewInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { HandGestureService } from 'tensorflow-web-gesture';
 
@@ -7,10 +8,17 @@ import { HandGestureService } from 'tensorflow-web-gesture';
   styleUrls: ['./app.component.css'],
   providers: [],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'lib-appliance';
+  handGestureService: HandGestureService;
 
-  public constructor(public handGestureService: HandGestureService) {
+  public constructor() {
+    this.handGestureService = new HandGestureService();
+  }
+  ngAfterViewInit(): void {
+    const vid = <HTMLVideoElement>document.getElementById('videoElement');
+
+    this.handGestureService.init(vid);
     this.handGestureService.subscribers.right.subscribe(() =>
       console.log('derecha')
     );
