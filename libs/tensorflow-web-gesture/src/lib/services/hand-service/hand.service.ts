7a6063999af6ce07f8cf09edcd7c134abc52ac1a
jose.hernandezr1 <jose.hernandez.rizo@rwth-aachen.de>
2021-06-16 17:58:54 +0200

    return to working main

--- libs/tensorflow-web-gesture/src/lib/services/hand-service/hand.service.ts
+++ libs/tensorflow-web-gesture/src/lib/services/hand-service/hand.service.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import '@tensorflow/tfjs-backend-webgl';
 import { BehaviorSubject } from 'rxjs';
 import * as handpose from '@tensorflow-models/handpose';
 import { GestureEstimation } from './hand-gesture.service';
@@ -6,8 +7,6 @@ import { MovementEstimation } from './hand-movement.service';
 import { Direction, Gesture, Subscribers } from '../../shared/utils';
 import { filter } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import * as handTrack from 'handtrackjs';
-import { HandTrack } from './hand-track.service';
 
 //The data manager
 @Injectable()
@@ -15,20 +14,16 @@ export class HandGestureService {
   private video: HTMLVideoElement;
   private movementEstimator: MovementEstimation;
   private gestureEstimator: GestureEstimation;
-  private handTracker: HandTrack;
 
   private _swipe$ = new BehaviorSubject<Direction>('none');
   private swipe$ = this._swipe$.asObservable();
-
   private _gesture$ = new BehaviorSubject<Gesture>('none');
   private gesture$ = this._gesture$.asObservable();
-
   public subscribers: Subscribers;
   constructor() {
     this.gestureEstimator = new GestureEstimation();
     this.initializeSubscribers();
   }
-
   init(vid: HTMLVideoElement) {
     vid.addEventListener(
       'loadeddata',
@@ -38,7 +33,6 @@ export class HandGestureService {
           vid.width,
           vid.height,
         ]);
-        this.handTracker = new HandTrack([vid.width, vid.height]);
         this.runModel();
         // Video is loaded and can be played
       },
@@ -59,53 +53,32 @@ export class HandGestureService {
     };
   }
 
-  private runModel() {
-    handTrack
-      .load()
-      .then((model) => {
-        this.handTrackDetection(model);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+  runModel() {
     handpose
       .load()
       .then((model) => {
-        this.handMovementDetection(model);
+        //Start data processing system -> Video procesor
+        const runDetection = () => {
+          model.estimateHands(this.video).then((predictions) => {
+            if (predictions && predictions[0]) {
+              //Run gesture detection
+              this.gestureEstimator.estimateGestures(
+                predictions[0].landmarks,
+                this._gesture$
+              );
+              //Run movement detection
+              this.movementEstimator.estimateHand(
+                predictions[0].boundingBox,
+                this._swipe$
+              );
+            }
+            requestAnimationFrame(runDetection);
+          });
+        };
+        runDetection();
       })
       .catch((err) => {
         console.error(err);
       });
-  }
-  private handTrackDetection(model: any): void {
-    const runHandTrackDetection = () => {
-      model.detect(this.video).then((predictions) => {
-        this.handTracker.estimateHand(predictions, this._swipe$);
-        requestAnimationFrame(runHandTrackDetection);
-      });
-    };
-    runHandTrackDetection();
-  }
-
-  private handMovementDetection(model: handpose.HandPose): void {
-    //Start data processing system -> Video procesor
-    const runDetection = () => {
-      model.estimateHands(this.video).then((predictions) => {
-        if (predictions.length > 0) {
-          //Run gesture detection
-          this.gestureEstimator.estimateGestures(
-            predictions[0].landmarks,
-            this._gesture$
-          );
-          //Run movement detection
-          // this.movementEstimator.estimateHand(
-          //   predictions[0].boundingBox,
-          //   this._swipe$
-          // );
-        }
-        requestAnimationFrame(runDetection);
-      });
-    };
-    runDetection();
   }
 }
